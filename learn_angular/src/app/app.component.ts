2055e98e917c3d24f76d91d70f0c0ef74f7ccedf
Vasiliy Prokudin <vd.prokudin@jet.su>
2023-04-17 13:55:21 +0300

    added components and data from api

--- learn_angular/src/app/app.component.ts
+++ learn_angular/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import type { IProduct } from './models/product';
+
+import { products as data } from './data/products';
+import { ProductsService } from './services/products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'learn_angular';
+export class AppComponent implements OnInit {
+  title = 'learn angular';
+  products: IProduct[] = [];
+  loading = false;
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+      this.loading = false;
+    });
+  }
 }
